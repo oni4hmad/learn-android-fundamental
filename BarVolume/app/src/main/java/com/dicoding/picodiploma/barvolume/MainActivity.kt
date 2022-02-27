c61677d091e7dd881d09b59837a7c082c653fad8
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-02-27 22:49:47 +0700

    initial commit

---
+++ BarVolume/app/src/main/java/com/dicoding/picodiploma/barvolume/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.dicoding.picodiploma.barvolume
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

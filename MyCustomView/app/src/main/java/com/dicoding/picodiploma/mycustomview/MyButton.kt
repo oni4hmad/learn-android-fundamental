df7614612c015788eee545560bae2155af5304d1
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-06 17:45:16 +0700

    custom view

---
+++ MyCustomView/app/src/main/java/com/dicoding/picodiploma/mycustomview/MyButton.kt
@@ -0,0 +1,61 @@
+package com.dicoding.picodiploma.mycustomview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.util.AttributeSet
+import android.view.Gravity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
+
+class MyButton : AppCompatButton {
+
+    private var enabledBackground: Drawable? = null
+    private var disabledBackground: Drawable? = null
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+
+        // Mengubah background dari Button
+        background = when {
+            isEnabled -> enabledBackground
+            else -> disabledBackground
+        }
+
+        // Mengubah warna text pada button
+        setTextColor(txtColor)
+
+        // Mengubah ukuran text pada button
+        textSize = 12f
+
+        // Menjadikan object pada button menjadi center
+        gravity = Gravity.CENTER
+
+        // Mengubah text pada button pada kondisi enable dan disable
+        text = when {
+            isEnabled -> "Submit"
+            else -> "Isi Dulu"
+        }
+    }
+
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button)
+        disabledBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable)
+
+    }
+
+}

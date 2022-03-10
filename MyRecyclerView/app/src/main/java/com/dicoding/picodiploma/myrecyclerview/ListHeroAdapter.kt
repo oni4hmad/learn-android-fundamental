a01098de284f000fb4c85d1cc4d2d46f5ba8e2f8
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-10 07:03:31 +0700

    ListHeroAdapter view binding

--- MyRecyclerView/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/ListHeroAdapter.kt
+++ MyRecyclerView/app/src/main/java/com/dicoding/picodiploma/myrecyclerview/ListHeroAdapter.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.dicoding.picodiploma.myrecyclerview.databinding.ItemRowHeroBinding
 
 class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
@@ -18,9 +19,11 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
     }
 
     /* membuat ViewHolder baru yang berisi layout item yang digunakan */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListHeroAdapter.ListViewHolder {
-        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
-        return ListViewHolder(view)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListHeroAdapter.ListViewHolder {
+        /*val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_row_hero, parent, false)
+        return ListViewHolder(view)*/ // ganti view binding
+        val binding = ItemRowHeroBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ListViewHolder(binding)
     }
 
     /* menetapkan data yang ada ke dalam ViewHolder sesuai dengan posisinya dengan menggunakan listHero[position]
@@ -29,11 +32,11 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
         val (name, description, photo) = listHero[position]
         /*holder.imgPhoto.setImageResource(photo)*/
         Glide.with(holder.itemView.context)
-            .load(photo)            // URL Gambar
-            .circleCrop()           // Mengubah image menjadi lingkaran
-            .into(holder.imgPhoto)  // imageView mana yang akan diterapkan
-        holder.tvName.text = name
-        holder.tvDescription.text = description
+            .load(photo)                        // URL Gambar
+            .circleCrop()                       // Mengubah image menjadi lingkaran
+            .into(holder.binding.imgItemPhoto)  // imageView mana yang akan diterapkan
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemDescription.text = description
 
         /*holder.itemView.setOnClickListener {
             Toast.makeText(holder.itemView.context, "Kamu memilih " + listHero[holder.adapterPosition].name, Toast.LENGTH_SHORT).show()
@@ -50,10 +53,11 @@ class ListHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adap
     *   tempat untuk menginisialisasi setiap komponen pada layout item dengan menggunakan itemView.findViewById.
     *   Adapter akan membuat objek ViewHolder seperlunya, serta menetapkan data untuk tampilan tersebut.
     *   Proses yang mengatribusikan tampilan ke datanya disebut binding.  */
-    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    class ListViewHolder(var binding: ItemRowHeroBinding) : RecyclerView.ViewHolder(binding.root) {
+        /*var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
         var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)
+        var tvDescription: TextView = itemView.findViewById(R.id.tv_item_description)*/ // ganti view binding
+
     }
 
     interface OnItemClickCallback {

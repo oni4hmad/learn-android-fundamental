71f643734a719d49bffdcbf44725606137901338
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-10 07:39:00 +0700

    build apk dg custom keystore

---
+++ MyApkApplication/app/src/main/java/com/dicoding/picodiploma/myapkapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.picodiploma.myapkapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

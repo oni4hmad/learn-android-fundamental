8994b9cbdbaf6d28fe714c6901b17fa46fcf8f2b
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-11 01:49:33 +0700

    submission 1.0

---
+++ MySubmission1/app/src/main/java/com/dicoding/picodiploma/mysubmission1/MainActivity.kt
@@ -0,0 +1,71 @@
+package com.dicoding.picodiploma.mysubmission1
+
+import android.content.Intent
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.picodiploma.mysubmission1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val list = ArrayList<User>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_MySubmission1)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Github User's"
+
+        binding.rvUsers.setHasFixedSize(true)
+        list.addAll(listUsers)
+        showRecyclerList()
+    }
+
+    private val listUsers: ArrayList<User>
+        get() {
+            val username = resources.getStringArray(R.array.username)
+            val name = resources.getStringArray(R.array.name)
+            val location = resources.getStringArray(R.array.location)
+            val company = resources.getStringArray(R.array.company)
+            val avatar = resources.obtainTypedArray(R.array.avatar)
+            val following = resources.getStringArray(R.array.following)
+            val followers = resources.getStringArray(R.array.followers)
+            val repository = resources.getStringArray(R.array.repository)
+
+            val listUser = ArrayList<User>()
+            for (i in username.indices) {
+                val user = User(username[i], name[i], location[i], company[i], avatar.getResourceId(i, -1), following[i], followers[i], repository[i])
+                listUser.add(user)
+            }
+            avatar.recycle()
+            return listUser
+        }
+
+    private fun showSelectedUser(user: User) {
+        val moveWithObjectIntent = Intent(this@MainActivity, UserDetailsActivity::class.java)
+        moveWithObjectIntent.putExtra(UserDetailsActivity.EXTRA_USER, user)
+        startActivity(moveWithObjectIntent)
+    }
+
+    private fun showRecyclerList() {
+        if (applicationContext.resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            binding.rvUsers.layoutManager = GridLayoutManager(this, 2)
+        } else {
+            binding.rvUsers.layoutManager = LinearLayoutManager(this)
+        }
+
+        val listUserAdapter = ListUserAdapter(list)
+        binding.rvUsers.adapter = listUserAdapter
+
+        listUserAdapter.setOnItemClickCallback(object : ListUserAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: User) {
+                showSelectedUser(data)
+            }
+        })
+    }
+}
---
+++ MySubmission1/app/src/main/java/com/dicoding/picodiploma/mysubmission1/UserDetailsActivity.kt
@@ -0,0 +1,42 @@
+package com.dicoding.picodiploma.mysubmission1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.dicoding.picodiploma.mysubmission1.databinding.ActivityUserDetailsBinding
+
+class UserDetailsActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserDetailsBinding
+
+    companion object {
+        const val EXTRA_USER = "extra_user"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.title = "Detail User"
+
+        val user = intent.getParcelableExtra<User>(EXTRA_USER) as User
+        binding.imgUser.setImageResource(user.avatar)
+        binding.tvName.text = user.name
+        binding.tvUsername.text = getString(R.string.username, user.username)
+        binding.tvLocation.text = user.location
+        binding.tvUserCount.text = getString(R.string.user_count, user.followers, user.following, user.repository)
+        binding.btnShare.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, "https://github.com/${binding.tvUsername.text.drop(1)}")
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+}

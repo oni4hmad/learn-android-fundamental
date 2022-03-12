03a9864be6d2005ec934e64b2db5625553332898
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-12 21:04:53 +0700

    navigation

---
+++ MyNavigation/app/src/main/java/com/dicoding/picodiploma/mynavigation/DetailCategoryFragment.kt
@@ -0,0 +1,47 @@
+package com.dicoding.picodiploma.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dicoding.picodiploma.mynavigation.databinding.FragmentCategoryBinding
+import com.dicoding.picodiploma.mynavigation.databinding.FragmentDetailCategoryBinding
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding: FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        /*return inflater.inflate(R.layout.fragment_detail_category, container, false)*/
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container,  false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /* get data from bundle */
+        /*val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
+        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)*/
+
+        /* get data from SafeArgs */
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}

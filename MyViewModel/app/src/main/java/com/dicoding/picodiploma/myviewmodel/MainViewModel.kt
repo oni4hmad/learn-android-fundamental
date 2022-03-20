8d5134e6fc98e4e5ba099e3de914f6fdab934f23
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-21 00:22:16 +0700

    latihan view model done

--- MyViewModel/app/src/main/java/com/dicoding/picodiploma/myviewmodel/MainViewModel.kt
+++ MyViewModel/app/src/main/java/com/dicoding/picodiploma/myviewmodel/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.dicoding.picodiploma.myviewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    var result = 0
+
+    fun calculate(width: String, height: String, length: String) {
+        result = width.toInt() * height.toInt() * length.toInt()
+    }
 }

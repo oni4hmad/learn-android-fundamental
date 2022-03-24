ce5af972b54cf08abe84783c275f4e5e4f5b6d38
Oni Ahmad <46712776+oni4hmad@users.noreply.github.com>
2022-03-24 23:12:44 +0700

    submission 2

---
+++ MySubmission2/app/src/main/java/com/dicoding/picodiploma/mysubmission2/ListUserAdapter.kt
@@ -0,0 +1,40 @@
+package com.dicoding.picodiploma.mysubmission2
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dicoding.picodiploma.mysubmission2.databinding.ItemRowUserBinding
+
+class ListUserAdapter(private val listUser: ArrayList<User>) : RecyclerView.Adapter<ListUserAdapter.ListViewHolder>() {
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
+        val binding = ItemRowUserBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+        return ListViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val (username, name, location, _, avatar) = listUser[position]
+        holder.binding.imgUser.setImageResource(avatar)
+        holder.binding.tvItemName.text = name
+        holder.binding.tvItemUsername.text = holder.itemView.context.getString(R.string.username, username)
+        holder.binding.tvItemLocation.text = location
+
+        holder.binding.imgUser.setOnClickListener { onItemClickCallback.onItemClicked(listUser[holder.adapterPosition]) }
+    }
+
+    override fun getItemCount(): Int = listUser.size
+
+    class ListViewHolder(var binding: ItemRowUserBinding) : RecyclerView.ViewHolder(binding.root)
+
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: User)
+    }
+
+}
